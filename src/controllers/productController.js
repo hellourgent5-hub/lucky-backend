30c54ded47fcc014c82bb120f3979d0ecbbf81b2
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-11 11:04:12 +0530

    Update productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,12 +1,24 @@
 const Product = require('../models/Product');
 
-const getProducts = async (req, res) => {
+// Create product
+async function createProduct(req, res) {
+  try {
+    const { name, price, category, description } = req.body;
+    const product = await Product.create({ name, price, category, description });
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+// Get all products
+async function getProducts(req, res) {
   try {
     const products = await Product.find();
     res.json(products);
-  } catch (error) {
-    res.status(500).json({ message: 'Server Error' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-};
+}
 
-module.exports = { getProducts };
+module.exports = { createProduct, getProducts };
