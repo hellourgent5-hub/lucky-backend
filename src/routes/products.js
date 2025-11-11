87d77c4fb1c62944dab43d3f709eb8dc34e6ce8f
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-11 07:29:55 +0530

    Delete src/routes/products.js

--- src/routes/products.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ success: true, message: 'Products route working 🛒' });
-});
-
-module.exports = router;
