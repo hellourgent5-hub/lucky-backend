af630fd07be5c174f6be146578de3eb03e95a1b9
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-11 08:23:09 +0530

    Delete src directory

--- src/models/Vendor.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const vendorSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  description: String,
-  active: { type: Boolean, default: true }
-});
-module.exports = mongoose.model('Vendor', vendorSchema);
--- src/routes/admin.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/auth');
-const { stats } = require('../controllers/adminController');
-
-router.get('/stats', auth('admin'), stats);
-module.exports = router;
