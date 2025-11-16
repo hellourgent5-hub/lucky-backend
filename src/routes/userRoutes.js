300031599e5b8b46c7d4e78f800b90928a9f0a4a
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-16 20:34:12 +0530

    Delete src directory

--- src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser, getUsers } = require('../controllers/userController');
-
-router.post('/register', registerUser);   // POST /api/users/register
-router.post('/login', loginUser);         // POST /api/users/login
-router.get('/', getUsers);                // GET /api/users
-
-module.exports = router;
